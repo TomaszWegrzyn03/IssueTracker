d8c4170791476dc4e06e872095accba8d9273f66
TomaszWegrzyn03 <tomaszwegrzyn.contact@gmail.com>
2023-01-12 20:50:28 +0100

    Adding Dtos.

---
+++ issuetracker-api/src/main/java/com/example/IssueTracker/issue/IssueGetDto.java
@@ -0,0 +1,32 @@
+package com.example.IssueTracker.issue;
+
+
+import com.example.IssueTracker.user.SimpleUserDto;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class IssueGetDto {
+
+    private Long issueId;
+
+    private String issueTitle;
+
+    private String issueDesc;
+
+    private String issueType;
+
+    private String issueStatus;
+
+    private String issuePriority;
+
+    private Long projectId;
+
+    private List<SimpleUserDto> users;
+
+}
